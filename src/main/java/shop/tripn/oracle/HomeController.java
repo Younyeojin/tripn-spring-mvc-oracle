d6fc8ba9d86c88345c9041c128ea2c25b110249b
Younyeojin <yjyoon2107@gmail.com>
2021-09-13 15:43:36 +0900

    0913

--- src/main/java/shop/tripn/oracle/HomeController.java
+++ src/main/java/shop/tripn/oracle/HomeController.java
@@ -1,22 +1,33 @@
 package shop.tripn.oracle;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+
 //import java.text.DateFormat;
 //import java.util.Date;
 import java.util.Locale;
 
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
-
+@SessionAttributes("contextPath")
 @Controller
 public class HomeController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		
+	public String home(
+			HttpSession session,
+			HttpServletRequest request) {
+		String contextPath = request.getContextPath();
+		session.setAttribute("contextPath", contextPath);
 		return "index";
 	}
 	@RequestMapping(value="/loginform", method = RequestMethod.GET)
@@ -29,7 +40,7 @@ public class HomeController {
 	}
 }
 
-//private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+//
 //logger.info("Welcome home! The client locale is {}.", locale);
 //
 //Date date = new Date();
--- src/main/webapp/WEB-INF/views/user/Join.jsp
+++ src/main/webapp/WEB-INF/views/user/Join.jsp
@@ -72,18 +72,20 @@ span.psw {
 
 <h2>회원가입 폼</h2>
 
-<form action="/action_page.php" method="post">
-  <div class="imgcontainer">
+<form action="${contextPath}/customers/join" method="post">
+  <!--<div class="imgcontainer">
     <img src="img_avatar2.png" alt="Avatar" class="avatar">
-  </div>
+  </div>  -->
 
   <div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-        
+    <label for="custId"><b>custId</b></label>
+    <input type="text" placeholder="Enter ID" name="custId" required>
+    <label for="custName"><b>custName</b></label>
+    <input type="text" placeholder="Enter Name" name="custName" required>
+    <label for="address"><b>address</b></label>
+    <input type="text" placeholder="Enter address" name="address" required>
+    <label for="phone"><b>phone</b></label>
+    <input type="text" placeholder="Enter phone" name="phone" required>    
     <button type="submit">Join</button>
     <label>
       <input type="checkbox" checked="checked" name="remember"> Remember me
