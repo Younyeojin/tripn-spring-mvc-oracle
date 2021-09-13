407aafb0ace5a7d244272431d4251b2501555eb6
Younyeojin <yjyoon2107@gmail.com>
2021-09-13 14:30:28 +0900

    0913

--- src/main/java/shop/tripn/oracle/HomeController.java
+++ src/main/java/shop/tripn/oracle/HomeController.java
@@ -1,11 +1,11 @@
 package shop.tripn.oracle;
 
-import java.text.DateFormat;
-import java.util.Date;
+//import java.text.DateFormat;
+//import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,22 +14,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
 		
 		return "index";
 	}
-	
+	@RequestMapping(value="/loginform", method = RequestMethod.GET)
+	public String login(Locale locle, Model model) {
+		return "/user/Login";
+	}
+	@RequestMapping(value="/joinform", method = RequestMethod.GET)
+	public String join(Locale locle, Model model) {
+		return "/user/Join";
+	}
 }
+
+//private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+//logger.info("Welcome home! The client locale is {}.", locale);
+//
+//Date date = new Date();
+//DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//
+//String formattedDate = dateFormat.format(date);
+//
+//model.addAttribute("serverTime", formattedDate );
--- src/main/java/shop/tripn/oracle/customer/controller/CustomerController.java
+++ src/main/java/shop/tripn/oracle/customer/controller/CustomerController.java
@@ -1,8 +1,17 @@
 package shop.tripn.oracle.customer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/customers")
 public class CustomerController {
-
+	@RequestMapping("/join")
+	public String join() {
+		return "회원가입 성공";
+	}
+	@RequestMapping("/login")
+	public String login() {
+		return "회원가입 성공";
+	}
 }
